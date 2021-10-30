6c4071ae16dd40be45999a9e8a96e1d113e7a587
Владимир Филимоненко <zrnrd@mail.ru>
2021-10-30 20:26:17 +0500

    Added display of the current time

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-let currentCity = localStorage.getItem("currentCity") || 'Тюмень';
+let currentCity = localStorage.getItem("currentCity") || 'Москва';
 
 const getWheather = async () => {
-    let temperature = document.querySelector(".temperature");
+    const temperature = document.querySelector(".temperature");
+    const currentTime = document.querySelector(".current-time");
 
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=85d9877a19499d8c389f1fb40ddcbdb4`);
     
-    let wheather;
 
     if(response.ok){
-        wheather = await response.json();
+        let wheather = await response.json();
+        let currentTemperature = (wheather.main.temp -273.15).toFixed(1);
+
+        temperature.innerText = getTemperature(currentTemperature);
+        currentTime.innerText = "Сейчас " + getCurrentTime();
+
     }
 
-    let currentTemperature = (wheather.main.temp -273.15).toFixed(1);
-
-    temperature.innerText = getTemperature(currentTemperature);
+    
 
 }
 
 const getTemperature = (temperature) => {
-    if(temperature === 0){
-        return 0;
-    }else if(temperature > 0){
+    if(temperature > 0){
         return "+" + temperature + "°C"
     }else{
-        return "-" + temperature + "°C"
+        return temperature + "°C"
     }
+}
+
+const getCurrentTime = () => {
+    const date = new Date();
+    return date.getHours() + ":" + date.getMinutes();
 }
 
 getWheather()
