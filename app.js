d866e2e3aa9b30ae1d0721da2cad9f4aa0445521
Владимир Филимоненко <zrnrd@mail.ru>
2021-11-04 08:35:22 +0500

    added wind direction function

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 let currentCity = localStorage.getItem("currentCity") || 'Москва';
 
+// Основная функция запроса погоды
 const getWheather = async () => {
     const cityName = document.querySelector(".city-name");
     const temperature = document.querySelector(".temperature");
     const currentTime = document.querySelector(".current-time");
+    const wind = document.querySelector(".wind");
+    const humidity = document.querySelector(".humidity");
 
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=85d9877a19499d8c389f1fb40ddcbdb4`);
 
@@ -14,14 +17,15 @@ const getWheather = async () => {
         cityName.innerText = currentCity;
         temperature.innerText = getCorrectTemperature(currentTemperature);
         currentTime.innerText = "Сейчас " + getCurrentTime();
+        wind.innerText = getCorrectWind(wheather.wind);
+        humidity.innerText = wheather.main.humidity + "%";
         localStorage.setItem("currentCity", currentCity);
     }else{
         alert("Ошибка запроса погоды");
     }
-    
-
 }
 
+// Вспомогательные функции
 const getCurrentTime = () => {
     const date = new Date();
     return date.getHours() + ":" + date.getMinutes();
@@ -34,10 +38,28 @@ const getCorrectTemperature = (temperature) => {
         return temperature + "°C"
     }
 }
-const getCorrectWind = (wind) => {
-    
+
+const getCorrectWind = ({speed, deg}) => {
+    if(deg >= 335 && deg <= 25){
+        return speed + " м/с, C" 
+    }else if(deg > 25 && deg < 65){
+        return speed + " м/с, CВ" 
+    }else if(deg >= 65 && deg <= 115){
+        return speed + " м/с, В" 
+    }else if(deg > 115 && deg < 155){
+        return speed + " м/с, ЮВ" 
+    }else if(deg >= 155 && deg <= 205){
+        return speed + " м/с, Ю" 
+    }else if(deg > 205 && deg < 245){
+        return speed + " м/с, ЮЗ" 
+    }else if(deg >= 245 && deg <= 295){
+        return speed + " м/с, З" 
+    }else{
+        return speed + " м/с, CЗ" 
+    }
 }
 
+// Повесить функцию на событие "после полной загрузки документа"
 getWheather()
 
 document.querySelector(".get-weather").addEventListener("click", () => {
